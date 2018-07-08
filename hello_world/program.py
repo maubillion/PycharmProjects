002332cf0f9822d4aaf5f32263152d26df5fc41c
maubillion <32743088+maubillion@users.noreply.github.com>
2018-07-08 12:36:25 +0000

    Initial commit

---
+++ hello_world/program.py
@@ -0,0 +1,8 @@
+print('------------------------------------------------------')
+print('                     Hello Africa')
+print('------------------------------------------------------')
+print()
+
+location = input('Where in Africa are you from? ')
+response = location + 'ian'' girls are the best :-)'
+print(response)
